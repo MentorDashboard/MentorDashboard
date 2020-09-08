a81be81a4797d6eb4d7a718be3e30359af088c6a
Mark Railton <mark@markrailton.com>
2020-09-08 19:10:05 +0100

    update tests to use TestConfig

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,13 @@ from src import create_app
 from config import Config
 
 
+class TestConfig(Config):
+    FLASK_ENV = 'testing'
+    TESTING = True
+
+
 @pytest.fixture(scope='module')
 def test_app():
-    app = create_app(Config)
+    app = create_app(TestConfig)
     with app.app_context():
         yield app

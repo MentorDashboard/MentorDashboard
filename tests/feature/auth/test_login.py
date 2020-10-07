4cad98b202ba935529d52666debf3a96057630c6
Mark Railton <mark@markrailton.com>
2020-10-07 15:22:56 +0100

    Updates auth tests to use new test_client

--- tests/feature/auth/test_login.py
+++ tests/feature/auth/test_login.py
@@ -1,11 +1,10 @@
 from ...utils import add_user, login_user
 
 
-def test_user_can_login(test_app, test_db):
-    client = test_app.test_client()
+def test_user_can_login(test_client, test_db):
     add_user("Test User", "user@test.com", "test1234")
 
-    response = client.post(
+    response = test_client.post(
         "/login",
         data=dict(
             email="user@test.com",
@@ -19,10 +18,8 @@ def test_user_can_login(test_app, test_db):
     assert "You have been logged in" in result
 
 
-def test_non_existent_user_can_not_login(test_app, test_db):
-    client = test_app.test_client()
-
-    response = client.post(
+def test_non_existent_user_can_not_login(test_client, test_db):
+    response = test_client.post(
         "/login",
         data=dict(
             email="user@test.com",
@@ -35,11 +32,10 @@ def test_non_existent_user_can_not_login(test_app, test_db):
     assert "Invalid email address or password" in result
 
 
-def test_user_can_not_login_with_incorrect_password(test_app, test_db):
-    client = test_app.test_client()
+def test_user_can_not_login_with_incorrect_password(test_client, test_db):
     add_user("Test User", "user@test.com", "test1234")
 
-    response = client.post(
+    response = test_client.post(
         "/login",
         data=dict(
             email="user@test.com",
@@ -52,10 +48,8 @@ def test_user_can_not_login_with_incorrect_password(test_app, test_db):
     assert "Invalid email address or password" in result
 
 
-def test_user_can_not_login_without_email_address(test_app, test_db):
-    client = test_app.test_client()
-
-    response = client.post(
+def test_user_can_not_login_without_email_address(test_client, test_db):
+    response = test_client.post(
         "/login",
         data=dict(
             password="WRONG_PASSWORD",
@@ -66,10 +60,9 @@ def test_user_can_not_login_without_email_address(test_app, test_db):
     assert "This field is required." in result
 
 
-def test_logged_in_user_can_not_access_login_page(test_app, test_db):
-    client = test_app.test_client()
-    login_user(client)
+def test_logged_in_user_can_not_access_login_page(test_client, test_db):
+    login_user(test_client)
 
-    response = client.get("/login")
+    response = test_client.get("/login")
 
     assert response.status_code == 302

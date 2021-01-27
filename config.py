0c0d7a17f135c70c5a9eba7f8f842a292e5a80ac
Simen Daehlin <simen.dehlin@gmail.com>
2021-01-27 20:33:57 +0000

    Updated config file to combat timeouts

--- config.py
+++ config.py
@@ -9,6 +9,11 @@ class Config(object):
     FLASK_ENV = os.getenv("FLASK_ENV") or "production"
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_ENGINE_OPTIONS = {
+    'connect_args': {
+        'connect_timeout': 1000
+    }
+}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BCRYPT_LOG_ROUNDS = 12
     SENTRY_DSN = os.environ.get("SENTRY_DSN")
